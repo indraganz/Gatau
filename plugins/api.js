dbb2f6eb4af17e2a5072a09206f1663428e5a633
Indraxyz <141424803+indraganz@users.noreply.github.com>
2024-08-18 17:51:39 +0700

    Update api.js

--- plugins/api.js
+++ plugins/api.js
@@ -143,6 +143,552 @@ async function fetchJson(url, options = {}) {
 		.catch(err => reject({code: 503, status: false, result: err }))
 	})
    }
+exports.snapsave = async (url) => {
+  return new Promise(async (resolve) => {
+    try {
+      function decodeSnapApp(args) {
+        let [h, u, n, t, e, r] = args;
+        function decode(d, e, f) {
+          const g = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
+          let h = g.slice(0, e);
+          let i = g.slice(0, f);
+          let j = d.split('').reverse().reduce(function (a, b, c) {
+            if (h.indexOf(b) !== -1)
+              return a += h.indexOf(b) * (Math.pow(e, c));
+          }, 0);
+          let k = '';
+          while (j > 0) {
+            k = i[j % f] + k;
+            j = (j - (j % f)) / f;
+          }
+          return k || '0';
+        }
+        r = '';
+        for (let i = 0, len = h.length; i < len; i++) {
+          let s = "";
+          while (h[i] !== n[e]) {
+            s += h[i]; i++;
+          }
+          for (let j = 0; j < n.length; j++)
+            s = s.replace(new RegExp(n[j], "g"), j.toString());
+          r += String.fromCharCode(decode(s, e, 10) - t);
+        }
+        return decodeURIComponent(encodeURIComponent(r));
+      }
+
+      function getEncodedSnapApp(data) {
+        return data.split('decodeURIComponent(escape(r))}(')[1]
+          .split('))')[0]
+          .split(',')
+          .map(v => v.replace(/"/g, '').trim());
+      }
+
+      function getDecodedSnapSave(data) {
+        return data.split('getElementById("download-section").innerHTML = "')[1]
+          .split('"; document.getElementById("inputData").remove(); ')[0]
+          .replace(/\\(\\)?/g, '');
+      }
+
+      function decryptSnapSave(data) {
+        return getDecodedSnapSave(decodeSnapApp(getEncodedSnapApp(data)));
+      }
+
+      const html = await got.post('https://snapsave.app/action.php?lang=id', {
+        headers: {
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'content-type': 'application/x-www-form-urlencoded',
+          'origin': 'https://snapsave.app',
+          'referer': 'https://snapsave.app/id',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        },
+        form: { url }
+      }).text();
+
+      const decode = decryptSnapSave(html);
+      const $ = cheerio.load(decode);
+      const results = [];
+
+      if ($('table.table').length || $('article.media > figure').length) {
+        const thumbnail = $('article.media > figure').find('img').attr('src');
+        $('tbody > tr').each((_, el) => {
+          const $el = $(el);
+          const $td = $el.find('td');
+          const resolution = $td.eq(0).text();
+          let _url = $td.eq(2).find('a').attr('href') || $td.eq(2).find('button').attr('onclick');
+          const shouldRender = /get_progressApi/ig.test(_url || '');
+          if (shouldRender) {
+            _url = /get_progressApi\('(.*?)'\)/.exec(_url || '')?.[1] || _url;
+          }
+          results.push({
+            resolution,
+            thumbnail,
+            url: _url,
+            shouldRender
+          });
+        });
+      } else {
+        $('div.download-items__thumb').each((_, tod) => {
+          const thumbnail = $(tod).find('img').attr('src');
+          const btns = $(tod).next('div.download-items__btn'); // Asumsi tombol berada tepat setelah thumbnail
+          btns.each((_, ol) => {
+            let _url = $(ol).find('a').attr('href');
+            if (!/https?:\/\//.test(_url || '')) _url = `https://snapsave.app${_url}`;
+            results.push({
+              thumbnail,
+              url: _url
+            });
+          });
+        });
+      }
+
+      if (!results.length) return resolve({ msg: `Blank data` });
+      return resolve({ data: results });
+    } catch (e) {
+      return resolve({ msg: e.message });
+    }
+  });
+}
+
+
+exports.ttstalk = async (username) => {
+	let retryCount = 0;
+	while (retryCount < 3) {
+		try {
+			const response = await axios.get(`https://tiktok.com/@${username}`);
+			const $ = cheerio.load(response.data);
+			const userData = JSON.parse($('#__UNIVERSAL_DATA_FOR_REHYDRATION__').text()).__DEFAULT_SCOPE__['webapp.user-detail'].userInfo;
+			
+			const userInfo = {
+				data: {
+					...userData.user,
+					...userData.stats
+				}
+			};
+			
+			return userInfo;
+		} catch (err) {
+			retryCount++;
+		}
+	}
+	return { error: true };
+}
+
+exports.mediafire = async (url) => {
+const res = await axios.get(url) 
+const $ = cheerio.load(res.data)
+const hasil = []
+const link = $('a#downloadButton').attr('href')
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+const seplit = link.split('/')
+const nama = seplit[5]
+mime = nama.split('.')
+mime = mime[1]
+hasil.push({ nama, mime, size, link })
+return hasil
+}
+
+exports.tiktok = async (url) => {
+    try {
+    const apiUrl = 'https://snapdouyin.app/wp-json/aio-dl/video-data/';
+
+    const headers = {
+    "content-type": "application/x-www-form-urlencoded",
+}
+
+        const params = new URLSearchParams();
+        params.append('url', url);
+
+        const response = await fetch(apiUrl, { method: "POST", body: params, headers });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+
+        const responseData = await response.json();
+
+        return responseData;
+    } catch (error) {
+        throw new Error(`An error occurred: ${error.message}`);
+    }
+}
+
+exports.completions = async (messages) => {
+
+    const options = {
+        provider: g4f.providers.GPT,
+        model: "gpt-4-32k-0314",
+        debug: true,
+    };
+
+    const json = await g4f.chatCompletion(messages, options);
+    return json;
+}
+
+
+exports.gpt4 = async (text) => {
+  const currentTime = moment().tz('Asia/Jakarta').format('LLLL');
+    
+    const messages = [
+        {
+            role: "system",
+            content: `Saya adalah asisten virtual yang dikembangkan oleh OpenAI dengan basis gpt-4 32k. Saya dirancang untuk membantu Anda dengan pertanyaan dan informasi yang Anda perlukan. Saat ini adalah ${currentTime}. Ada yang bisa saya bantu?`
+        },
+        {
+            role: "user",
+            content: text
+        }
+    ];
+
+    const options = {
+        provider: g4f.providers.GPT,
+        model: "gpt-4-32k-0314",
+        debug: true,
+    };
+
+    const json = await g4f.chatCompletion(messages, options);
+    return json;
+}
+
+exports.pixiv = async (text) => {
+    return axios.get("https://api.lolicon.app/setu/v2?size=regular&r18=0&num=20&keyword=" + text)
+        .then(data => data.data.data);
+}
+
+exports.pixivr18 = async (text) => {
+    return axios.get("https://api.lolicon.app/setu/v2?size=regular&r18=1&num=20&keyword=" + text)
+        .then(data => data.data.data);
+}
+
+exports.gemini = async (inputText) => {
+    // For text-only input, use the gemini-pro model
+    const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + googlekey; 
+    const data = {
+        contents: [{
+            parts: [{
+                text: inputText
+            }]
+        }]
+    };
+    const response = await axios.post(url, data, {
+        headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+    console.log(response.data);
+    return response.data.candidates[0].content.parts[0].text;
+}
+
+exports.geminiVision = async (inputTextt, inputImage) => {
+	const bup = await Resize(inputImage)
+	const requestBody = {
+		"contents": [
+
+			{
+				"parts": [
+
+					{
+						"text": inputTextt
+					},
+
+					{
+						"inline_data": {
+							"mime_type": "image/jpeg",
+							"data": bup.toString('base64')
+						}
+					}
+
+				]
+			}
+
+		]
+	};
+	const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${googlekey}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	});
+	const data = await response.json();
+	console.log(data);
+	return data.candidates[0].content.parts[0].text;
+}
+
+exports.blackbox = async (content, web) => {
+    try {
+        const response = await fetch("https://www.blackbox.ai/api/chat", {
+            method: "POST",
+            headers: {
+                "Accept": "*/*",
+                "Accept-Language": "id-ID,en;q=0.5",
+                "Referer": "https://www.blackbox.ai/",
+                "Content-Type": "application/json",
+                "Origin": "https://www.blackbox.ai",
+                "Alt-Used": "www.blackbox.ai"
+            },
+            body: JSON.stringify({
+                messages: [{ role: "user", content }],
+                id: "chat-free",
+                previewToken: null,
+                userId: "",
+                codeModelMode: true,
+                agentMode: {},
+                trendingAgentMode: {},
+                isMicMode: false,
+                userSystemPrompt: "You are BlackBox Ai, a useful AI Model for millions of developers using Blackbox Code Chat that will answer coding questions and help them when writing code.",
+                maxTokens: 1024,
+                webSearchMode: web,
+                promptUrls: "",
+                isChromeExt: false,
+                githubToken: null
+            })
+        });
+
+        let result = await response.text();
+
+        if (web) {
+            const jsonMatch = result.match(/\$\~\~\~\$(.*?)\$\~\~\~\$/);
+            if (jsonMatch) {
+                const jsonData = JSON.parse(jsonMatch[1]);
+                const formattedJson = jsonData.slice(0, 2).map(item => 
+                    `• *Title* : ${item.title}\n• *Link* : ([${item.link}])`
+                ).join('\n\n');
+
+                result = result.replace(/\$\~\~\~\$.*\$\~\~\~\$/, '') + '\n\n### *Referensi Sumber* : \n' + formattedJson;
+            }
+        }
+
+        result = result.replace(/\$@\$\s?v=v\d+\.\d+-rv\d+\$\@\$Sources:.+\n\n/, '');
+        return result.trim();
+    } catch (error) {
+        return { creator: creator,
+            status: false,
+            msg: error.message
+         }
+    }
+};
+
+exports.toVideo = async (str) => {
+      return new Promise(async resolve => {
+         try {
+            const image = Buffer.isBuffer(str) ? str : str.startsWith('http') ? await (await axios.get(str, {
+               responseType: 'arraybuffer'
+            })).data : str
+            let form = new FormData
+            form.append('new-image', Buffer.from(image), 'image.webp')
+            form.append('new-image-url', '')
+            const html = await (await axios.post('https://ezgif.com/webp-to-mp4', form, {
+               headers: {
+                  "Accept": "*/*",
+                  "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+                  "Origin": "https://ezgif.com",
+                  "Referer": "https://ezgif.com/webp-to-mp4",
+                  "Referrer-Policy": "strict-origin-when-cross-origin",
+                  "sec-ch-ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
+                  "sec-ch-ua-platform": "Android",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "x-requested-with": "XMLHttpRequest",
+                  ...form.getHeaders()
+               }
+            })).data
+            const $ = cheerio.load(html)
+            let File = $('#main > form').find('input[type=hidden]:nth-child(1)').attr('value')
+            let token = $('#main > form').find('input[type=hidden]:nth-child(2)').attr('value')
+            let Submit = $('#tool-submit-button').find('input').attr('value')
+            const Format = {
+               file: File,
+               token: token,
+               convert: Submit
+            }
+            const proc = await (await axios({
+               url: "https://ezgif.com/webp-to-mp4/" + File,
+               method: "POST",
+               data: new URLSearchParams(Object.entries(Format)),
+               headers: {
+                  "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+                  "Origin": "https://ezgif.com",
+                  "Referer": "https://ezgif.com/webp-to-mp4",
+                  "Referrer-Policy": "strict-origin-when-cross-origin",
+                  "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                  "accept-language": "en-US,en;q=0.9,id;q=0.8",
+                  "content-type": "application/x-www-form-urlencoded",
+                  "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\""
+               }
+            })).data
+            const link = cheerio.load(proc)('#output > p.outfile').find('video > source').attr('src')
+            if (!link) return resolve({
+               creator: creator,
+               status: false,
+               msg: 'Failed to convert!'
+            })
+            resolve({
+               creator: creator,
+               status: true,
+               data: {
+                  url: 'https:' + link
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: creator,
+               status: false,
+               msg: e.message
+            })
+         }
+      })
+}
+
+exports.npm = async(text) => {
+  const response = await axios.get(`https://registry.npmjs.com/-/v1/search?text=${text}`);
+  return response.data;
+}
+
+exports.asupan = async(query) => {
+  try {
+    const response = await axios({
+      url: "https://tikwm.com/api/feed/search",
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "cookie": "current_language=en",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
+      },
+      data: {
+        "keywords": query,
+        "count": 12,
+        "cursor": 0,
+        "hd": 1
+      }
+    });
+
+    const videos = response.data.data.videos;
+    if (videos.length === 0) {
+      throw new Error("No videos found");
+    }
+
+    // Get a random video from the videos array
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    const randomVideo = videos[randomIndex];
+
+    // Format the random video data
+    const data = {
+      caption: randomVideo.title,
+      author: { ...randomVideo.author, username: randomVideo.author.unique_id },
+      stats: {
+        play_count: randomVideo.play_count,
+        digg_count: randomVideo.digg_count,
+        share_count: randomVideo.share_count,
+        comment_count: randomVideo.comment_count,
+      },
+      music: randomVideo.music_info,
+      duration: randomVideo.duration,
+      video: randomVideo.play,
+    };
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+exports.rotate = async(url, type) => {
+  try {
+    let form = new URLSearchParams();
+    form.append('new-image-url', url);
+    form.append('submit', 'Upload!');
+
+    let initialResponse = await axios.post('https://ezgif.com/rotate', form);
+    let $ = cheerio.load(initialResponse.data);
+    let data = [];
+
+    $('img').each((i, e) => {
+      let tmp = $(e).attr('src');
+      if (tmp.match(/(\/tmp\/)/g)) data.push(tmp);
+    });
+
+    if (data.length === 0) {
+      return {
+        creator: global.creator,
+        status: false
+      };
+    }
+
+    let input = {
+      target: $('form[class="form ajax-form"]').attr('action'),
+      filename: data[0].split('/')[4],
+      token: $('input[name="token"]').attr('value')
+    };
+
+    let rotateForm = new URLSearchParams();
+    rotateForm.append('file', input.filename);
+    rotateForm.append('token', input.token);
+    rotateForm.append(type, 'on');
+    rotateForm.append('free_deg', 45);
+    rotateForm.append('submit', 'Upload!');
+
+    let rotateResponse = await axios.post(input.target, rotateForm);
+    $ = cheerio.load(rotateResponse.data);
+
+    let rotatedImageUrl = $('p.outfile > img').attr('src');
+
+    return {
+      creator: global.creator,
+      status: true,
+      data: {
+        url: 'https:' + rotatedImageUrl
+      }
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      creator: global.creator,
+      status: false
+    };
+  }
+}
+
+exports.nikParse = async(nik) => {
+  try {
+    const { data } = await axios.post(
+      'https://indonesia-ktp-parser-validator.p.rapidapi.com/ktp_validator',
+      { nik },
+      {
+        headers: {
+          'x-rapidapi-key': '9e0c59de57msh3b88e80e31c66c4p1b85e5jsn48b379c99680',
+          'x-rapidapi-host': 'indonesia-ktp-parser-validator.p.rapidapi.com',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (data?.result?.data) {
+      const result = data.result.data;
+
+      const today = new Date();
+      const birthDate = new Date(result.lahir.split('/').reverse().join('-'));
+      let age = today.getFullYear() - birthDate.getFullYear();
+      if (today < new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate())) {
+        age--;
+      }
+      result.umur = `${age} tahun`;
+
+      const posData = await axios.get(`https://kodepos.vercel.app/search/?q=${encodeURIComponent(result.kecamatan)}`);
+      const lokasi = posData?.data?.data?.[0] || {};
+      result.kode_pos = lokasi.code || 'Tidak ditemukan';
+      result.latitude = lokasi.latitude || null;
+      result.longitude = lokasi.longitude || null;
+
+      return data;
+    }
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 exports.pin = async (querry) => {
 	return new Promise((resolve,reject) => {
