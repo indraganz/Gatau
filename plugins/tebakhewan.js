d8fee797c21bce1ea0e3bf944e43ac637b8280a7
Indraxyz <141424803+indraganz@users.noreply.github.com>
2024-08-24 09:14:15 +0700

    Update tebakhewan.js

--- plugins/tebakhewan.js
+++ plugins/tebakhewan.js
@@ -1,9 +1,6 @@
-const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const path = require('path');
-
-const app = express();
 
 const tebakHewan = async () => {
     const randomPageNumber = Math.floor(Math.random() * 5) + 1; // Assuming there are 5 pages
@@ -24,11 +21,10 @@ const tebakHewan = async () => {
             };
         }).get();
 
+        // Randomly select one animal
         const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
-        return {
-            creator: global.creator,
-            ...randomAnimal
-        };
+
+        return randomAnimal;
     } catch (error) {
         throw new Error(`Failed to fetch data: ${error.message}`);
     }
